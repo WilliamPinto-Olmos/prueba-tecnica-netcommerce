e19f7cea5286690d7becd0b74a322bcd994e70d1
WilliamPinto-Olmos <williamp.olmos@gmail.com>
2023-11-02 21:56:28 -0600

    build: generado recurso de task.

---
+++ database/migrations/2023_11_03_034929_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->text('description')->nullable();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('company_id')->constrained('companies');
+            $table->boolean('is_completed')->default(false);
+            $table->dateTime('start_at')->nullable();
+            $table->dateTime('expired_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
