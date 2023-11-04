2fb77bcf13cd5f47d197c72672050cf488a6a628
WilliamPinto-Olmos <williamp.olmos@gmail.com>
2023-11-03 20:58:04 -0600

    feat: habilitada api de companies.

---
+++ app/Http/Requests/FetchCompaniesRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
+class FetchCompaniesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'search' => 'nullable|string|max:255',
+        ];
+    }
+
+    /**
+     * Apparently, even when laravel docs says that when you make an
+     * API request it automatically returns a JSON response, it doesn't. This overrides
+     * the default behaviour and forces a JSON response.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(
+            response()->json([
+                'errors' => $errors
+            ], 
+            JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
